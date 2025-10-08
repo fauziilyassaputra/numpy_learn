e62a70b0794490ccfe40e609bf4d14770178b2b7
fauzi ilyas saputra <fauziilyassaputra@gmail.com>
2025-10-08 08:43:43 +0700

    Update _05.databaru_dari_datalama.py

--- _05.databaru_dari_datalama.py
+++ _05.databaru_dari_datalama.py
@@ -39,4 +39,45 @@ print("=================================")
 # mengambil data yang sudah ada
 array_kita = numpy.loadtxt('data.csv', delimiter=',', dtype=numpy.float32)
 print(array_kita)
+print("shape :", array_kita.shape)
+print("tipe data :", array_kita.dtype)
 
+
+print("=================================")
+# simpan array dengan format tertentu
+
+numpy.array([1,2,3,4], dtype=numpy.int32).tofile('data.bin') # buat data.bin
+load_data = numpy.fromfile('data.bin', dtype=numpy.int32)
+print(load_data)
+
+
+print("=================================")
+# membuat array dengan build in python (range)
+array_kita = numpy.array(range(10))
+print("array kita dengan range : ", array_kita)
+
+print("=================================")
+# membuat array dengan generator
+
+def generator_data(n):
+    for i in range(n):
+        yield i ** 2
+
+# array_kita = numpy.array(list(generator_data(5)))
+array_kita = numpy.fromiter(generator_data(5), dtype=numpy.int32, count=5)
+print("hasil dari array generator: ", array_kita)
+
+print("=================================")
+# membuat array berdasarkan  buffer
+
+data_kita = b'\x00\x00\x00\x00\x00\xf0\x00@'
+array_kita = numpy.frombuffer(data_kita, dtype=numpy.float64)
+print("array_kita dari buffer : ", array_kita)
+
+print("=================================")
+# membuat array berdasarkan  dict
+data_kita = {'x': [1,2], 'y': [3,4]}
+values = list(data_kita.values())
+array_kita = numpy.array(values)
+print("array_kita dari dict: \n", array_kita)
+print("dimensi array_kita: ", array_kita.shape)
